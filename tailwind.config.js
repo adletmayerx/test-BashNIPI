ae34983131d5d4954d9f4be4d6930d5e37d0f0e7
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-08 07:00:20 +0500

    feat: add c\project colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: "#696969",
+        "light-grey": "#696969",
+        yellow: "#FFD200",
+        gold: "#FFD300",
+        corduroy: "#5F7465",
+        tapa: "#727270",
+      },
+    },
   },
   plugins: [],
-}
-
+};
