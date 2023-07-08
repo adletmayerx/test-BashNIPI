cda6293a486bd35dacb0f64a0c4bff5a493336da
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-08 16:55:43 +0500

    feat: add Accordion

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
 import AccordionItem from "./AccordionItem.vue";
+import User from "../types/User";
 
-defineProps();
+type Props = {
+  users: Array<User>;
+};
+
+defineProps<Props>();
 </script>
 
 <template>
-    <ul class="gap-3">
-      <li>
-        <AccordionItem name="Leanne Graham" email="Sincere@april.biz" phone="1-770-736-8031 x56442" website="hildegard.org" />
+  <div class="py-8 px-14">
+    <div class="p-3 pl-10 flex">
+      <ul class="flex justify-between text-grey text-sm grow ml-5 gap-8">
+        <li>
+          <p> Name </p>
+        </li>
+        <li>
+          <p> Email </p>
+        </li>
+        <li>
+          <p> Phone </p>
+        </li>
+        <li>
+          <p> Website </p>
+        </li>
+      </ul>
+      <div class="grow shrink"></div>
+    </div>
+    <ul class="flex flex-col gap-3 ">
+      <li v-for="user in users" :key="user.id">
+        <AccordionItem :user="user" />
       </li>
     </ul>
+  </div>
 </template>
