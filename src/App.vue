ab3aedc40d982f94a51e761268838fbf902ae71e
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-08 16:56:06 +0500

    feat: fetch and render data

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,32 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
 import Header from "./components/Header.vue";
 import Accordion from "./components/Accordion.vue";
+import { onBeforeMount, ref } from "vue";
+
+const users = ref<Array<any>>([]);
+
+onBeforeMount(async () => {
+  const cachedUsers = localStorage.getItem("users");
+
+  if (cachedUsers) {
+    users.value = JSON.parse(cachedUsers);
+
+    return;
+  }
+
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    users.value = data;
+    localStorage.setItem("users", JSON.stringify(data));
+  } catch (e) {
+    console.error(e);
+  }
+});
 </script>
 
 <template>
-  <!-- <div class="bg-black border">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
   <Header />
-  <Accordion />
+  <Accordion v-if="users.length" :users="users" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
