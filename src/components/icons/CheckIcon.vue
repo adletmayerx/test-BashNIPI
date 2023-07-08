0a439a6d80ec92d45ce3d940ff4af6b2da3cc1e4
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-08 15:43:44 +0500

    feat: add CheckIcon

---
+++ src/components/icons/CheckIcon.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps();
+</script>
+
+<template>
+  <svg
+    :style="{ width: 42 + 'px', height: 42 + 'px' }"
+    viewBox="0 0 42 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    :props="props"
+  >
+    <rect width="41.6923" height="40" rx="4" transform="matrix(-1 0 0 1 41.6924 0)" fill="#FFD300" />
+    <path
+      d="M12.5078 20.4894L19.7633 28.0293C20.2284 28.5126 21.0281 28.41 21.3561 27.8249L30.227 12"
+      stroke="#5F7465"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+import CheckIcon from "./CheckIcon.vue";
+import DownArrowIcon from "./DownArrowIcon.vue";
+
+export { CheckIcon, DownArrowIcon };
