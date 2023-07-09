ff54b1604a9b5973cf7ca48ad68f0b6bd1c34316
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-09 13:41:24 +0500

    fix: fix event name

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -44,22 +44,22 @@ const onInputChange = ({ name, value, block }: InputChangeParams) => {
     </div>
     <div class="grid grid-cols-3 border border-light-grey rounded-b">
       <fieldset class="flex flex-col gap-3 py-5 px-8">
-        <LabelVue block="address" name="street" :value="user.address.street" @inputChange="onInputChange" />
-        <LabelVue block="address" name="suite" :value="user.address.suite" @inputChange="onInputChange" />
-        <LabelVue block="address" name="city" :value="user.address.city" @inputChange="onInputChange" />
-        <LabelVue block="address" name="zipcode" :value="user.address.zipcode" @inputChange="onInputChange" />
+        <LabelVue block="address" name="street" :value="user.address.street" @inputchange="onInputChange" />
+        <LabelVue block="address" name="suite" :value="user.address.suite" @inputchange="onInputChange" />
+        <LabelVue block="address" name="city" :value="user.address.city" @inputchange="onInputChange" />
+        <LabelVue block="address" name="zipcode" :value="user.address.zipcode" @inputchange="onInputChange" />
       </fieldset>
       <fieldset class="flex flex-col gap-3 py-5 px-8 border-x border-light-grey">
-        <LabelVue block="company" name="name" :value="user.company.name" @inputChange="onInputChange" />
-        <LabelVue block="company" name="catchPhrase" :value="user.company.catchPhrase" @inputChange="onInputChange" />
-        <LabelVue block="company" name="bs" :value="user.company.bs" @inputChange="onInputChange" />
+        <LabelVue block="company" name="name" :value="user.company.name" @inputchange="onInputChange" />
+        <LabelVue block="company" name="catchPhrase" :value="user.company.catchPhrase" @inputchange="onInputChange" />
+        <LabelVue block="company" name="bs" :value="user.company.bs" @inputchange="onInputChange" />
       </fieldset>
       <fieldset class="flex flex-col gap-3 py-5 px-8">
-        <LabelVue :block="null" name="name" :value="user.name" @inputChange="onInputChange" />
-        <LabelVue :block="null" name="username" :value="user.username" @inputChange="onInputChange" />
-        <LabelVue :block="null" name="email" :value="user.email" @inputChange="onInputChange" />
-        <LabelVue :block="null" name="phone" :value="user.phone" @inputChange="onInputChange" />
-        <LabelVue :block="null" name="website" :value="user.website" @inputChange="onInputChange" />
+        <LabelVue :block="null" name="name" :value="user.name" @inputchange="onInputChange" />
+        <LabelVue :block="null" name="username" :value="user.username" @inputchange="onInputChange" />
+        <LabelVue :block="null" name="email" :value="user.email" @inputchange="onInputChange" />
+        <LabelVue :block="null" name="phone" :value="user.phone" @inputchange="onInputChange" />
+        <LabelVue :block="null" name="website" :value="user.website" @inputchange="onInputChange" />
       </fieldset>
     </div>
   </div>
