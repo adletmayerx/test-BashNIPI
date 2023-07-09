4631a67981b1f7ca3d0a7a8638d0c2d68b613e0d
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-09 07:13:48 +0500

    feat: add emit event on input change

--- src/components/shared/LabelVue.vue
+++ src/components/shared/LabelVue.vue
@@ -5,11 +5,22 @@ type Props = {
 };
 
 defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: 'inputChange', name: string, value: string): void
+}>()
+
+const handleInputChange = (e: Event) => {
+  const input = e.target as HTMLInputElement;
+
+  emit('inputChange', input.name.toLowerCase(), input.value);
+}
 </script>
 
 <template>
   <label class="inline-flex flex-col gap-2">
     <span class="text-xs opacity-50 capitalize">{{ name }}</span>
-    <input :name="name" type="text" class="border rounded py-2 px-4 text-sm border-light-grey" :value="value" />
+    <input :name="name" type="text" class="border rounded py-2 px-4 text-sm border-light-grey" :value="value"
+      @change="handleInputChange" />
   </label>
 </template>
