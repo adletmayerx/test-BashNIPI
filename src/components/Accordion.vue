2e6ad5d3e500385b8c4aa1be9152766650b2f672
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-09 07:15:59 +0500

    style: style table

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -13,14 +13,15 @@ defineProps<Props>();
 
 <template>
   <div class="py-8 px-14">
-    <table class="w-full border-separate border-spacing-0 text-grey ">
-      <thead class="text-sm">
-        <th class="w-10"></th>
-        <th v-for="heading in HEADINGS" :key="heading" class="text-left"> {{ heading }} </th>
-        <th class="w-10"></th>
+    <table class="w-full border-separate border-spacing-0 text-grey overflow-hidden">
+      <thead class="text-xs text-grey bg-white relative z-10">
+        <th class="w-10 "></th>
+        <th v-for="heading in HEADINGS" :key="heading" class="text-left opacity-50 font-normal py-2"> {{ heading }} </th>
+        <th class="w-10 py-3"></th>
       </thead>
       <tbody>
-        <AccordionItem :key="users[0].id" :user="users[0]" />
+        <!-- <AccordionItem :key="users[0].id" :user="users[0]" /> -->
+        <AccordionItem v-for="user in users" :key="user.id" :user="user" />
       </tbody>
     </table>
   </div>
