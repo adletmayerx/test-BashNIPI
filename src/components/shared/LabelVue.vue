bcf30398992bf15173f3721790e5f115d95c8666
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-09 13:05:50 +0500

    feat: typed input change emit

--- src/components/shared/LabelVue.vue
+++ src/components/shared/LabelVue.vue
@@ -1,26 +1,44 @@
 <script setup lang="ts">
-type Props = {
-  name: string;
+type InputChangeParamsBasic = {
+  block: null;
+  name: "name" | "username" | "email" | "phone" | "website";
   value: string;
 };
+type InputChangeParamsAddress = {
+  block: "address";
+  name: "street" | "suite" | "city" | "zipcode";
+  value: string;
+};
+type InputChangeParamsCompany = {
+  block: "company";
+  name: "name" | "catchPhrase" | "bs";
+  value: string;
+};
+export type InputChangeParams = InputChangeParamsBasic | InputChangeParamsCompany | InputChangeParamsAddress;
 
-defineProps<Props>();
+const { block, name, value } = defineProps<InputChangeParams>();
 
 const emit = defineEmits<{
-  (e: 'inputChange', name: string, value: string): void
-}>()
+  (e: "inputChange", params: InputChangeParams): void;
+}>();
 
 const handleInputChange = (e: Event) => {
   const input = e.target as HTMLInputElement;
+  const params: InputChangeParams = { block, name, value: input.value } as InputChangeParams;
 
-  emit('inputChange', input.name.toLowerCase(), input.value);
-}
+  emit("inputChange", params);
+};
 </script>
 
 <template>
   <label class="inline-flex flex-col gap-2">
     <span class="text-xs opacity-50 capitalize">{{ name }}</span>
-    <input :name="name" type="text" class="border rounded py-2 px-4 text-sm border-light-grey" :value="value"
-      @change="handleInputChange" />
+    <input
+      :name="name"
+      type="text"
+      class="border rounded py-2 px-4 text-sm border-light-grey"
+      :value="value"
+      @change="handleInputChange"
+    />
   </label>
 </template>
