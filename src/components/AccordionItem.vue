a9a412e39609f4cdec413524c3cb71d70e16d281
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-09 07:15:35 +0500

    feat: add details row visibility toggling logic

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -2,18 +2,26 @@
 import { DownArrowIcon, CheckIcon } from "./icons";
 import Details from "./Details.vue";
 import User from "../types/User";
+import { ref } from "vue";
 
 type Props = {
   user: User;
 };
 
 defineProps<Props>();
+
+const isDetailsVisible = ref(false)
+
+const handleDownArrowButtonClick = () => {
+  isDetailsVisible.value = !isDetailsVisible.value;
+}
+
 </script>
 
 <template>
-  <tr>
-    <td class="p-4 rounded-l border border-r-0 border-yellow">
-      <button type="button">
+  <tr class="text-sm bg-white z-10 relative">
+    <td class="p-2 rounded-l border border-r-0 border-yellow">
+      <button type="button" @click="handleDownArrowButtonClick">
         <DownArrowIcon />
       </button>
     </td>
@@ -23,14 +31,18 @@ defineProps<Props>();
     <td class="border-t border-b border-yellow">{{ user.website }}</td>
     <td class="border border-l-0 rounded-r border-yellow"></td>
   </tr>
-  <tr class="">
-    <td colspan="6">
-      <div class="flex items-start py-3 px-9 gap-3">
-        <Details :user="user"></Details>
-        <button type="button">
-          <CheckIcon />
-        </button>
-      </div>
-    </td>
-  </tr>
+  <Transition enter-active-class="duration-300 ease-out" enter-from-class="transform opacity-0 translate-x-full"
+    enter-to-class="opacity-100 translate-x-0" leave-active-class="duration-200 ease-in" leave-from-class="opacity-100 translate-x-0"
+    leave-to-class="transform opacity-0 translate-x-full">
+    <tr v-if="isDetailsVisible" class="bg-white relative">
+      <td colspan="6">
+        <div class="flex items-start py-3 px-9 gap-3">
+          <Details :user="user"></Details>
+          <button type="button">
+            <CheckIcon />
+          </button>
+        </div>
+      </td>
+    </tr>
+  </Transition>
 </template>
