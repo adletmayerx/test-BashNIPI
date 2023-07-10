f64e5424dfe34c392c6bc9b627aadbd25336d326
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-10 16:18:50 +0500

    feat: add LabelVue tests

---
+++ src/components/__tests__/LabelVue.spec.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "vitest";
+import { LabelVue } from "../shared";
+import { mount } from "@vue/test-utils";
+
+describe("LabelVue", () => {
+  it("успешный рендер span", () => {
+    const wrapper = mount(LabelVue, { props: { block: "address", name: "bs", value: "string" } });
+    const span = wrapper.find("span");
+
+    expect(span.exists()).toBeTruthy();
+  });
+
+  it("успешный рендер input", () => {
+    const wrapper = mount(LabelVue, { props: { block: "address", name: "bs", value: "string" } });
+    const input = wrapper.find("input");
+
+    expect(input.exists()).toBeTruthy();
+  });
+
+  it("корректное отображение значения поля ввода", () => {
+    const wrapper = mount(LabelVue, { props: { block: "address", name: "bs", value: "string" } });
+    const input = wrapper.find("input");
+
+    expect(input.element.value).toMatch("string");
+  });
+
+  it("корректное отображение значения span", () => {
+    const wrapper = mount(LabelVue, { props: { block: "address", name: "bs", value: "string" } });
+    const span = wrapper.get("span");
+
+    expect(span.text()).toMatch("bs");
+  });
+});
