447bc0bf5b178816a662e7bc5e7b1986978e3626
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-10 16:43:12 +0500

    feat: use pinia

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import "./style.css";
 import App from "./App.vue";
 
 const pinia = createPinia();
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(pinia)
-app.mount('#app')
+app.use(pinia);
+app.mount("#app");
