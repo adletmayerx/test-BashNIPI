d667cdbc2f8a6ea6beced6713e1cbca7883ab379
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-12 04:51:19 +0500

    style: styled Accordion

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -12,12 +12,14 @@ const { users } = defineProps<Props>();
 </script>
 
 <template>
-  <div class="py-8 px-14">
-    <table class="w-full border-separate border-spacing-y-2 text-grey overflow-hidden">
-      <thead class="text-xs text-grey bg-white relative z-10">
-        <th class="w-10"></th>
-        <th v-for="heading in HEADINGS" :key="heading" class="text-left opacity-50 font-normal py-2">{{ heading }}</th>
-        <th class="w-10 py-3"></th>
+  <div class="container">
+    <table class="table">
+      <thead class="table-head">
+        <th class="table-head__heading table-head__heading_side"></th>
+        <th v-for="heading in HEADINGS" :key="heading" class="table-head__heading table-head__heading_main">
+          {{ heading }}
+        </th>
+        <th class="table-head__heading table-head__heading_side"></th>
       </thead>
       <tbody>
         <AccordionItem v-for="user in users" :key="user.id" :user="user" />
@@ -25,3 +27,39 @@ const { users } = defineProps<Props>();
     </table>
   </div>
 </template>
+
+<style scoped>
+.container {
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  padding-left: 3.5rem;
+  padding-right: 3.5rem;
+}
+
+.table {
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  color: var(--grey);
+  overflow: hidden;
+}
+
+.table-head {
+  font-size: 0.75rem;
+  line-height: 1rem;
+  background-color: white;
+  position: relative;
+  z-index: 2;
+}
+
+.table-head__heading_side {
+  width: 2.5rem;
+}
+
+.table-head__heading_main {
+  text-align: left;
+  opacity: 0.5;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+}
+</style>
