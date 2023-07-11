4cbf9d5df13e7a2d402498d0d3bd023633741ebf
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-12 04:31:58 +0500

    style: styled header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-  <header class="border-b border-tapa border-opacity-20 py-6 px-12">
-    <h1 class="text-xl font-medium">Users</h1>
+  <header class="header">
+    <h1 class="header__title">Users</h1>
   </header>
 </template>
+
+<style scoped>
+.header {
+  border-bottom: rgba(114, 114, 112, 0.2) solid 1px;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  padding-left: 3rem;
+  padding-right: 3rem;
+}
+
+.header__title {
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  font-weight: 500;
+}
+</style>
